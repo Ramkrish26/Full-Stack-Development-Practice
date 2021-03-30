82b15f532d9c475a433a28b79072251a8d30d05a
Ramkrish26 <ramakrishnan2629@gmail.com>
2021-03-30 22:47:40 +0530

    Day 3 ES6

--- ES6/ES6_files/2.functions_and_Block_scoping.js
+++ ES6/ES6_files/2.functions_and_Block_scoping.js
@@ -33,18 +33,32 @@
 // greet();
 
 //If we use let for function declaration then the scope will be like block scope
-console.log("Function block scope")
-let greet = function() {
-    console.log("Hi");
+// console.log("Function block scope")
+// let greet = function() {
+//     console.log("Hi");
+// }
+
+// greet();
+
+// {
+//     let greet = function() {
+//         console.log("Hello");
+//     }
+//     greet();
+// }
+
+// greet();
+
+// Default parameter values
+// ES5
+function add(a, b) {
+    // return (b == undefined ? a+1 : a+b);
+    return a + (b == undefined ? 1 : b);
 }
+console.log(add(5, 6));
+console.log(add(5));
 
-greet();
-
-{
-    let greet = function() {
-        console.log("Hello");
-    }
-    greet();
-}
-
-greet();
+// ES6 
+let sum = (x, y=1) => x + y;
+console.log(sum(5, 6));
+console.log(sum(5));
---
+++ ES6/ES6_files/6.destructuring.js
@@ -0,0 +1,33 @@
+//ES5
+var a = 2;
+var b = 3;
+console.log(a);
+console.log(b);
+
+//ES6 Destructuring
+var [c,d] = [2,3];
+console.log(c);
+console.log(d);
+
+//Swapping two numbers
+//ES5
+// var e = a;
+// a = b;
+// b = e;
+// console.log(a, b);
+
+//Es6 
+[a,b] = [b,a]
+console.log(a, b);
+
+//Default values for a variable if it doesnt't have any value assigned
+let list = ['Chennai','India'];
+
+//ES5
+// var city = list[0];
+// var country = list[1] === undefined ? 'USA' : list[1];
+// console.log(city, country);
+
+//ES6 
+[city, country = "USA"] = list;
+console.log(city, country);
