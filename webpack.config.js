0f62a0d23d6b38273ac05c8476bb81c392303243
Ramkrish26 <ramakrishnan2629@gmail.com>
2021-05-05 22:18:49 +0530

    Day 8 React JS

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// To configure Webpack to bundle your application into a single bundle file
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+  };
