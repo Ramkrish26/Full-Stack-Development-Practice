bc9e8d8341445d0e67da390d76497d7becd63ba5
ramkrish26 <ramakrishnan2629@gmail.com>
2022-06-15 07:24:25 +0530

    Variables and data types

---
+++ Javascript/javascript_files/3.variables.js
@@ -0,0 +1,10 @@
+let firstname = "ram";
+console.log(firstname);
+
+let country = "India";
+let continent = "Asia";
+let population = "1300000000"
+
+console.log(country);
+console.log(continent);
+console.log(population);
