8c1715c4cfb2d3a371054f77cf6d7b868ae281df
Ramakrishnan Rajaram <ramakrishnan2629@gmail.com>
2022-06-16 07:21:53 +0530

    String literals (#15)

---
+++ Javascript/javascript_files/5.string_literals.js
@@ -0,0 +1,10 @@
+const firstname = 'Ram';
+const job = 'Engineer';
+const birthYear = 1997;
+const year = 2022
+
+// const ram = "I'm "+firstname+ ', a '+(year-birthYear)+ ' years old ' + job +'!'
+
+const ramNew = `I'm ${firstname}, a ${year-birthYear} years old ${job}!`
+
+console.log(ramNew)
