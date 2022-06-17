5fee5611ae95e6b78b87eb4250e9c749613139ca
ramkrish26 <ramakrishnan2629@gmail.com>
2022-06-17 07:28:47 +0530

    Strict mode

---
+++ Javascript/javascript_files/6.strict_mode.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can driver');
+
+//const interface = 'Audio'; // Unexpected strict mode reserved word
