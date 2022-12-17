d30345ad2d4e9b77d12b1a6ffea029b8029e751f
Ramakrishnan Rajaram <ramakrishnan2629@gmail.com>
2022-12-17 21:03:53 +0530

    Javascript day 3 (#21)
    1. Nested Destructuring Array
    2. Mutating variables in object destructuring
    3. Spread operator
    4. Short-circuiting
    5. Nullish coalescing operator

---
+++ ES6/ES6_files/21.short_ciruiting.js
@@ -0,0 +1,11 @@
+// Logical operator || and && can be used for all data types
+
+// Logical OR will return the first trtuthy value
+console.log(2 || null) // 2
+console.log(0 || 2) // 2
+console.log(0 || null || 'Ram') // Ram
+
+// Logical AND will return the first falsy value
+console.log(2 && null) // null
+console.log(0 && 2) // 0
+console.log(0 && null && 'Ram') // 0
--- ES6/ES6_files/9.functions_rest_parameters.js
+++ ES6/ES6_files/9.functions_rest_parameters.js
@@ -1,21 +1,21 @@
 //ES5
 // If we are not sure how many arguments are passed
-function AddAll (){
-    var sum=0;
-    for(var i = 0; i< arguments.length;i++){
-        sum = sum+arguments[i];        
-    }   
+function AddAll() {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        sum = sum + arguments[i];
+    }
     console.log(sum);
 }
-AddAll(1,2,1,4,6);
+AddAll(1, 2, 1, 4, 6);
 
 // ES6
 // if we know the there is going to be one parameter followed by n number of parameter
-function AddAllNumbers (a,...x){
-    var sum=a;  
-    for(var i = 0; i< x.length;i++){
-        sum = sum+x[i];        
-    }   
+function AddAllNumbers(a, ...x) {
+    var sum = a;
+    for (var i = 0; i < x.length; i++) {
+        sum = sum + x[i];
+    }
     console.log(sum);
 }
-AddAllNumbers(4,2,1,4,6);
+AddAllNumbers(4, 2, 1, 4, 6);
