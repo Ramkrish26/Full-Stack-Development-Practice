605c11e300d2ebf0a59c56f7be8cda186ee3873e
ramkrish26 <ramakrishnan2629@gmail.com>
2022-12-25 21:53:58 +0530

    JavaScript Day 8 1. Building a promise

--- ES6/ES6_files/30.promises.js
+++ ES6/ES6_files/30.promises.js
@@ -1,17 +1,31 @@
-'use strict';
+// 'use strict';
 
-const request = fetch('https://restcountries.com/v3.1/name/peru')
-console.log(request);
+// // Consuming promises
+// const request = fetch('https://restcountries.com/v3.1/name/peru')
+// console.log(request);
 
-// Consuming promises. fetch returns a promise
-const getCountryData = function (country) {
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-        .then(function (response) {
-            // json() method also returns a promise
-            return response.json()
-        })
-        .then(data => console.log(data))
-        .catch(err => console.log(`${err} thrown`));
-}
+// // Consuming promises. fetch returns a promise
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.com/v3.1/name/${country}`)
+//         .then(function (response) {
+//             // json() method also returns a promise
+//             return response.json()
+//         })
+//         .then(data => console.log(data))
+//         .catch(err => console.log(`${err} thrown`));
+// }
 
-getCountryData('peru')
+// getCountryData('peru')
+
+// Creating a promise
+const lotteryPromise = new Promise(function (resolve, reject) {
+    if (Math.random() >= 0.5)
+        resolve('You won');
+    else
+        reject('You lost');
+})
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// Resolve a promise immediately
+Promise.resolve('abc').then(res => console.log(res));
